5c32387b54805b95e94cf81da702bac5f664ca1d
victorpmessias <victor.p.messias@gmail.com>
2022-02-26 20:26:49 -0300

    SelleDaoJDB and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
 		Department obj = new Department(1, "books");
 		
 		Seller seller = new Seller(1, "Victor", "victor@email.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
 		System.out.println(obj);
 		System.out.println(seller);
